7fe1de15e04d6aede69a63a3c2f20b4720f84478
kfisch2 <kmlindsey24@gmail.com>
2022-10-13 15:17:13 -0700

    add initial routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Resume from "./components/Resume";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="flex-colum justify-flex-start min-100-vh">
+        <Header />
+        <div className="page-height container">
+          <Routes>
+            <Route path={"/"} element={<Home />} />
+            <Route path={"/about"} element={<About />} />
+            <Route path={"/portfolio"} element={<Portfolio />} />
+            <Route path={"/contact"} element={<Contact />} />
+            <Route path={"/resume"} element={<Resume />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Home from './Home'
+import Header from './Header'
+import Contact from './Contact'
+import Footer from './Footer'
+import Nav from './Nav'
+import Project from './Project'
+import Portfolio from './Portfolio'
