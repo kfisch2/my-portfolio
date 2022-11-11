73e90e08538b3078e1a806b3b010860f70c36491
Kayla Fischer <102554319+kfisch2@users.noreply.github.com>
2022-11-11 15:14:54 -0800

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,10 @@ export default function About() {
       <div>
         I'm Kayla, a recent graduate from the University of Oregon Web
         Development Bootcamp. I have a background in mathematics, Japanese, and
-        teaching. Check out my previous projects in my portfolio, reach out to
+        teaching. I spent nearly 10 years in education, developing strong intepersonal 
+        skills that I believe can be beneficial to the tech field. I am excited about 
+        working with a variety of people with different backgrounds and skill sets.
+        Check out my previous projects in my portfolio, reach out to
         me on LinkedIn, or visit my github. Currently exploring software engineer 
         and web development jobs!
       </div>
