8758a4ba1d759ce3afeabef7669acdba36fb8b44
kfisch2 <kmlindsey24@gmail.com>
2023-01-16 12:48:42 -0800

    centered page content

--- src/components/About.js
+++ src/components/About.js
@@ -4,16 +4,16 @@ import portrait from "../images/portrait2.jpg";
 export default function About() {
   return (
     <section className="about">
-      <div>
+      <div className="portrait">
         {" "}
-        <img className="portrait" src={portrait} alt="Portrait of Kayla"></img>
+        <img src={portrait} alt="Portrait of Kayla"></img>
       </div>
       <div className="about-div">
         Software developer who recently graduated from a coding bootcamp with a
         degree in mathematics and 10 years of experience in teaching. Working in
         the preschool setting taught me many valuable lessons that I hold dearly
         and can carry into my next career as a software engineer. I am
-        passionate people, making web applications to enhance the user
+        passionate about people, making web applications to enhance the user
         experience, and continuing to learn new technologies. Check out my
         previous projects in my portfolio, reach out to me on LinkedIn, or visit
         my github!
